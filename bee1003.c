31500dc52184a33ed1329b2d4af344523b130c25
ellenolv <ellendeoliveirapereira>
2023-07-20 20:26:17 -0300

    adicionado arquivo bee1003

---
+++ bee1003.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, SOMA;
+
+    // Leitura dos valores de A e B
+
+    scanf("%d", &A);
+
+    scanf("%d", &B);
+
+    // Cálculo da soma
+    SOMA = A + B;
+
+    // Impressão do resultado
+    printf("SOMA = %d\n", SOMA);
+
+    return 0;
+}
